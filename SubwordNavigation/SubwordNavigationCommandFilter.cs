b5d1096e4247ce9992913f0e6481577300fb47e5
Ali Hosseini Khayat <ali-hk@users.noreply.github.com>
2019-03-09 13:13:26 -0800

    feat: support grow and shrink selection

--- SubwordNavigation/SubwordNavigationCommandFilter.cs
+++ SubwordNavigation/SubwordNavigationCommandFilter.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Text.Operations;
 using System;
@@ -55,6 +56,9 @@ namespace VisualStudio.SubwordNavigation {
 
             commands.Add(PkgCmdIDList.cmdidSubwordDeletePrevious, () => Delete(MovePrevious));
             commands.Add(PkgCmdIDList.cmdidSubwordDeleteNext, () => Delete(MoveNext));
+
+            commands.Add(PkgCmdIDList.cmdidSubwordShrinkSelection, () => ShrinkSelection());
+            commands.Add(PkgCmdIDList.cmdidSubwordGrowSelection, () => GrowSelection());
         }
 
         private void MovePrevious(bool extendSelection) {
@@ -128,6 +132,110 @@ namespace VisualStudio.SubwordNavigation {
                 select(true);
             }
             operations.Delete();
+        }
+
+        private void ShrinkSelection()
+        {
+            var caret = textView.Caret;
+
+            if (caret.InVirtualSpace)
+            {
+                return;
+            }
+
+            var point = caret.Position.BufferPosition;
+
+            if (point.Position == 0)
+            {
+                return;
+            }
+
+            if (textView.Selection.IsEmpty)
+            {
+                return;
+            }
+
+            var startExtent = navigator.GetExtentOfSubword(textView.Selection.Start.Position);
+            var endExtent = navigator.GetExtentOfSubword(textView.Selection.End.Position - 1);
+
+            if (startExtent == null || endExtent == null)
+            {
+                return;
+            }
+
+            if (startExtent.Value.Span == endExtent.Value.Span)
+            {
+                textView.Selection.Select(textView.Selection.ActivePoint, textView.Selection.ActivePoint);
+                return;
+            }
+
+            // Prefer shrinking other characters or words to shrinking the subword
+            char leftChar = startExtent.Value.Span.End.GetChar();
+            char rightChar = endExtent.Value.Span.Start.Subtract(1).GetChar();
+            var preferLeft = char.IsLetterOrDigit(leftChar) || leftChar == '_';
+            var preferRight = char.IsLetterOrDigit(rightChar) || rightChar == '_';
+
+            // Compare against caret, prefer to keep the subword under the caret selected
+            preferLeft &= startExtent.Value.Span.End >= point;
+            preferRight &= endExtent.Value.Span.Start <= point;
+
+            if ((preferLeft && preferRight) || (!preferLeft && !preferRight))
+            {
+                textView.Selection.Select(new SnapshotSpan(startExtent.Value.Span.End, endExtent.Value.Span.Start), false);
+            }
+            else if (preferLeft)
+            {
+                textView.Selection.Select(new SnapshotSpan(textView.Selection.Start.Position, endExtent.Value.Span.Start), false);
+            }
+            else
+            {
+                textView.Selection.Select(new SnapshotSpan(startExtent.Value.Span.End, textView.Selection.End.Position), false);
+            }
+        }
+        private void GrowSelection()
+        {
+            var caret = textView.Caret;
+
+            if (caret.InVirtualSpace)
+            {
+                return;
+            }
+
+            var point = caret.Position.BufferPosition;
+
+            if (point.Position == 0)
+            {
+                return;
+            }
+
+            var startPoint = textView.Selection.IsEmpty ? point : textView.Selection.Start.Position;
+            var endPoint = textView.Selection.IsEmpty ? point : textView.Selection.End.Position;
+            var startExtent = navigator.GetExtentOfSubword(startPoint - 1);
+            var endExtent = navigator.GetExtentOfSubword(endPoint);
+
+            if (startExtent == null || endExtent == null)
+            {
+                return;
+            }
+
+            // Prefer extending the subword over extending to include other characters or words
+            char leftChar = startExtent.Value.Span.End.Subtract(1).GetChar();
+            char rightChar = endExtent.Value.Span.Start.GetChar();
+            var isLeftLetterOrDigit = char.IsLetterOrDigit(leftChar) || leftChar == '_';
+            var isRightLetterOrDigit = char.IsLetterOrDigit(rightChar) || rightChar == '_';
+
+            if ((isLeftLetterOrDigit && isRightLetterOrDigit) || (!isLeftLetterOrDigit && !isRightLetterOrDigit))
+            {
+                textView.Selection.Select(new SnapshotSpan(startExtent.Value.Span.Start, endExtent.Value.Span.End), false);
+            }
+            else if (isLeftLetterOrDigit)
+            {
+                textView.Selection.Select(new SnapshotSpan(startExtent.Value.Span.Start, endPoint), false);
+            }
+            else
+            {
+                textView.Selection.Select(new SnapshotSpan(startPoint, endExtent.Value.Span.End), false);
+            }
         }
     }
 }
