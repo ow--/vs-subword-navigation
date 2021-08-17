20fdf7131761daba99fbff1e43b215e95a19d0fb
reduckted <reduckted@outlook.com>
2021-08-17 20:39:17 +1000

    Added support for VS 2022. Removed support for VS 2015 and lower.

--- SubwordNavigation/SubwordNavigationPackage.cs
+++ SubwordNavigation/SubwordNavigationPackage.cs
@@ -5,11 +5,11 @@ using System.Globalization;
 using System.Runtime.InteropServices;
 
 namespace VisualStudio.SubwordNavigation {
-    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration("#110", "#112", "1.5.0", IconResourceID = 400)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [Guid(GuidList.guidSubwordNavigationPkgString)]
-    public sealed class SubwordNavigationPackage : Package {
+    public sealed class SubwordNavigationPackage : AsyncPackage {
         public SubwordNavigationPackage() {
             Debug.WriteLine(string.Format(CultureInfo.CurrentCulture, "Entering constructor for: {0}", this.ToString()));
         }
